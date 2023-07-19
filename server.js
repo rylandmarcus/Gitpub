f5a323c598436234a70a4437056bcb408d9a0a58
ryland <ryland@MacBook-Pro-4.local>
2023-07-18 19:17:53 -0700

    added drinks as a list on the index and capitalized the first letter

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/', (req, res)=>{
 
 app.get('/drinks', (req, res)=>{
     // res.send(drinks)
-    res.render('drinks_index.ejs')
+    res.render('drinks_index.ejs', {
+        drinks: drinks
+    })
 })
 
 app.listen(port, ()=>{
