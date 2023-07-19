8492e07c3faf21b8a397346ed1b1a6275e381767
ryland <ryland@MacBook-Pro-4.local>
2023-07-18 18:19:59 -0700

    added the drinks into the server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const port = 3000
+const drinks = require('./models/drinks.js')
 
 app.get('/', (req, res)=>{
-    res.send('hey')
+    res.send('Welcome to the Gitpub App!')
 })
 
-app.listen(3000, ()=>{
-    console.log('this is working');
+app.get('/drinks', (req, res)=>{
+    res.send(drinks)
+})
+
+app.listen(port, ()=>{
+    console.log('this is working on port', port);
 })
