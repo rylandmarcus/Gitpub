4c521e9dd2664f8cf2535d27bbd32700b9651698
ryland <ryland@MacBook-Pro-4.local>
2023-07-22 16:41:20 -0700

    added the food menu

--- server.js
+++ server.js
@@ -2,22 +2,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 const drinks = require('./models/drinks.js')
+const food = require('./models/food.js')
 
 app.get('/', (req, res)=>{
     res.send('Welcome to the Gitpub App!')
 })
 
+//INDEX
 app.get('/drinks', (req, res)=>{
-    // res.send(drinks)
     res.render('drinks/drinks_index.ejs', {
         drinks: drinks
     })
 })
 
+app.get('/food', (req, res)=>{
+    res.render('food/food_index.ejs', {
+        food: food
+    })
+})
+
+//SHOW
 app.get('/drinks/:id', (req, res)=>{
-    // res.send(req.params.id)
     res.render('drinks/drinks_show.ejs', {
         drink: drinks[req.params.id]
+    })
+})
+
+app.get('/food/:id', (req, res)=>{
+    res.render('food/food_show.ejs', {
+        meal: food[req.params.id]
     })
 })
 
